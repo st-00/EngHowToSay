06fd429763aa22ead903601e2b54c79eb51a8907
St <steve00@yandex.ru>
2018-09-14 17:02:58 +0300

    SentenceParserTests updated

--- Tests/SentenceParserTests.cs
+++ Tests/SentenceParserTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using EngHowToSay;
-using OpenNLP.Tools.SentenceDetect;
 using Xunit;
 
 namespace Tests
@@ -15,71 +14,101 @@ namespace Tests
         [Fact]
         public void Parse_Sentences()
         {
-            string[] parts = Regex.Split(TestData.Topic0, @"(?<=[\.\?\!])");
+            var sentences = _sentenceParser.GetSentences(TestData.TextSentences);
 
-
-            var sentences = _sentenceParser.GetSentences(TestData.Topic1);
-
-            Assert.Equal(2, sentences.Length);
+            Assert.Equal(4, sentences.Length);
             foreach (var item in sentences)
             {
                 Assert.False(string.IsNullOrWhiteSpace(item));
             }
         }
 
+        [Fact]
+        public void Parse_Sentences_With_Slash()
+        {
+            var sentences = _sentenceParser.GetSentences(TestData.TextSentencesWithSlash);
+
+            Assert.Equal(3, sentences.Length);
+        }
 
         [Fact]
         public void Parse_Two_Sentences_With_Translations()
         {
-            
-
             List<SentenceModel> sentences = _sentenceParser.GetSentences(TestData.Topic1, TestData.Topic1Eng);
 
             Assert.Equal(2, sentences.Count);
-
 
             foreach (var item in sentences)
             {
                 Assert.False(string.IsNullOrWhiteSpace(item.Sentence));
                 Assert.False(string.IsNullOrWhiteSpace(item.Translation));
             }
-
         }
     }
 
     public class SentenceParser
     {
         public readonly string[] SentenceSeparators = {".", "?", "!"};
+        public readonly string SpecialSeparator3Forms = "/";
 
 
-        public string[] GetSentences(string sentences)
+        public string[] GetSentences(string textWithSentences)
         {
-            //return sentences.Split(SentenceSeparators, StringSplitOptions.RemoveEmptyEntries);
+            string[] sentences = Regex.Split(textWithSentences, @"(?<=[\.\?\!])")
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToArray();
 
-            // sentence min length?
+            var sentensesCombined = CombineSentences(sentences);
 
-            //for (var index = 0; index < sentences.Length; index++)
-            //{
-            //    var character = sentences[index];
-
-            //    if (SentenceSeparators.Contains(character))
-            //    {
-
-            //    }
-
-            //}
-
-            return null;
+            return sentensesCombined;
         }
 
+        private string[] CombineSentences(string[] sentences)
+        {
+            var sentensesCombined = new List<string>(sentences.Length);
+            string senteceBuffer = null;
+
+            for (int i = 0, iNext = 1; i < sentences.Length; i++, iNext++)
+            {
+                var sentence = sentences[i];
+                var sentenceNext = iNext < sentences.Length ? sentences[iNext] : string.Empty;
+
+                if (senteceBuffer == null)
+                {
+                    senteceBuffer = sentence;
+                }
+
+                if (sentenceNext.Contains(SpecialSeparator3Forms))
+                {
+                    senteceBuffer += sentenceNext;
+                }
+                else
+                {
+                    sentensesCombined.Add(senteceBuffer);
+                    senteceBuffer = null;
+                }
+            }
+
+            return sentensesCombined.ToArray();
+        }
 
         public List<SentenceModel> GetSentences(string sentences, string translations)
         {
-            return new List<SentenceModel>()
+            var a1 = GetSentences(sentences);
+            var a2 = GetSentences(translations);
+
+            if (a1.Length != a2.Length)
             {
-                new SentenceModel(){Sentence = "2", Translation = "2"},
-                new SentenceModel(){Sentence = "2", Translation = "2"},
-            };
+                throw new Exception("Sentences count doesn't match to Translations count!");
+            }
+
+            var result = new List<SentenceModel>();
+            for (int i = 0; i < a1.Length; i++)
+            {
+                result.Add(new SentenceModel() {Sentence = a1[i], Translation = a2[i]});
+            }
+
+            return result;
         }
     }
 }
--- Tests/TestData.cs
+++ Tests/TestData.cs
@@ -14,7 +14,9 @@ namespace Tests
 чинить велосипеды!       " + "before tab\tafter tab  ";
 
 
-        public const string Topic0 = @"Я умел чинить велосипеды. Ты умел воспитывать хулиганов.Я умел чинить велосипеды? Я умел чинить велосипеды!";
+        public const string TextSentences = @"Я умел чинить велосипеды. Ты умел воспитывать хулиганов.Я умел чинить велосипеды? Я умел чинить велосипеды!";
+        public const string TextSentencesWithSlash = @"I could repair bicycles. I could repair bicycles. / Could I repair bicycles? / I couldn't repair bicycles. 
+You could bring up hooligans. / Could you bring up hooligans? /You couldn't bring up hooligans.";
 
         public const string Topic1 = @"Я умел чинить велосипеды. Ты умел воспитывать хулиганов.";
         public const string Topic1Eng = "I could repair bicycles.   You couldn't bring up hooligans. ";
